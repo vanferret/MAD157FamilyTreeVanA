6f24c700545dd1d6c543da60344d2950e3997531
student <student@mbp-stud-10.local>
2020-02-23 23:02:25 -0600

    Initial Commit

---
+++ MAD157FamilyTreeVanA/MainNavigationController.swift
@@ -0,0 +1,16 @@
+//
+//  MainNavigationController.swift
+//  MAD157FamilyTreeVanA
+//
+//  Created by student on 2/23/20.
+//  Copyright © 2020 vanderson. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MainNavigationController: UINavigationController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
